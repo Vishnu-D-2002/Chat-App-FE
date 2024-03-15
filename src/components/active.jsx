ef584e671aac3eae4a33e22d47c45c0a43ee06ed
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-03-15 18:34:31 +0530

    active component css

--- src/components/active.jsx
+++ src/components/active.jsx
@@ -4,20 +4,17 @@ import { protecdInstance } from "../services/instance";
 
 function Active() {
   const [info, setInfo] = useState("");
-  const handleactive = async (e) => {
+
+  const handleActive = async (e) => {
     e.preventDefault();
     const currentURL = window.location.href;
     const match = currentURL.slice(-7);
 
     if (match) {
-        const activationToken = match;
-        console.log(activationToken);
+      const activationToken = match;
       try {
         const res = await protecdInstance.get(`/activate/${activationToken}`);
-        // console.log('Response:', res);
-
         if (res.data) {
-          console.log("Activation successful:", res.data);
           setInfo(res.data.message);
         } else {
           console.error("No data received in the response");
@@ -31,20 +28,28 @@ function Active() {
   };
 
   return (
-    <div>
-      <div className="m-5 d-flex flex-column ">
-        <h2>To activate your Account please click the link below,else ignore it. </h2>
-        <button
-          className="btn btn-success mx-auto mt-4"
-          onClick={handleactive}
-          id="btn"
-        >
-          Activate
-        </button>
-        <p className="fs-3 text-danger ">{info}</p>
-        <Link to="/" className="fs-3 text-decoration-none mx-auto">
-          Login
-        </Link>
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6">
+          <div className="card p-5">
+            <h2 className="mb-4">
+              To activate your account, please click the button below:
+            </h2>
+            <button
+              className="btn btn-success btn-block"
+              onClick={handleActive}
+            >
+              Activate
+            </button>
+            <p className="mt-3 text-danger fs-5 mb-0">{info}</p>
+            <p className="mt-3 text-center fs-5 mb-0">
+              Already have an account?{" "}
+              <Link to="/" className="text-decoration-none">
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
